67016f152fdb60b3b6bd5eeb47ab2ac1f65962d9
marko-panaiotov <108656266+marko-panaiotov@users.noreply.github.com>
2023-12-03 18:49:01 +0200

    Delete BoutiquePresentsWorkshop directory

--- BoutiquePresentsWorkshop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BoutiquePresentsWorkshop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
